41f63e2d130fe0c45f3f2ba16ad725a952b90292
Kutalo Alexey <kutalo84@gmail.com>
2019-04-12 19:33:02 -0700

    Login user

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './stylesheets/main.scss';
+import {appConfig} from './constants';
+import {UserSession} from 'blockstack';
+import { Button } from 'react-bulma-components';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          test
-      </div>
-    );
-  }
+    state = {
+        userSession: new UserSession({appConfig})
+    };
+
+    componentDidMount = async () => {
+        const { userSession } = this.state;
+        if (!userSession.isUserSignedIn() && userSession.isSignInPending()) {
+            const userData = await userSession.handlePendingSignIn();
+            if (!userData.username) {
+                throw new Error('This app requires a username');
+            }
+            window.location = '/'
+        }
+    };
+
+    handleSignIn = () => {
+        const {userSession} = this.state;
+        userSession.redirectToSignIn();
+    };
+
+    render() {
+        console.log(this.state.userSession);
+        const {userSession} = this.state;
+        return (
+            <div className="App">
+                {
+                    userSession.isUserSignedIn() ?
+                        <Button color="primary">
+                            Sign out
+                        </Button> :
+                        <Button color="primary" onClick={this.handleSignIn}>
+                            Sign in
+                        </Button>
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
